cf4acda3c681f46add5dc782cebeae1ff897ed11
DayOS <43686351+Day-OS@users.noreply.github.com>
2022-08-26 14:25:35 -0300

    First implementation of entities

---
+++ src/main/java/com/daytheipc/mundormo/Entities.java
@@ -0,0 +1,13 @@
+package com.daytheipc.mundormo;
+
+import org.bukkit.Location;
+
+public class Entities {
+    float breedingMultiplier;
+    float weightMultiplier; //HOW MUCH MEAT does it return after getting killed
+
+
+    public void _AreaCheck(Location location){
+
+    }
+}
--- src/main/java/com/daytheipc/mundormo/Mundormo.java
+++ src/main/java/com/daytheipc/mundormo/Mundormo.java
@@ -1,30 +1,41 @@
 package com.daytheipc.mundormo;
 
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 public final class Mundormo extends JavaPlugin implements Listener {
+    Entities entities = new Entities();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         Logger logger = getLogger();
-        logger.log(Level.WARNING, "you sheeet ya pants");
-        System.out.println("hiii :33");
+        logger.log(Level.WARNING, "hiii :33");
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onCreatureSpawn(EntitySpawnEvent e){
         getLogger().log(Level.INFO, "" + e.getEntity().getEntityId());
-        Player player = e.getLocation().getWorld().getPlayers().get(0);
-        player.sendMessage("aaa" +  e.getEntity().getEntityId());
 
+    }
+    @EventHandler
+    public void onInteractAtEntity(PlayerInteractAtEntityEvent e){
+        Entity entity = e.getRightClicked();
+        PersistentDataContainer data = entity.getPersistentDataContainer();
+        NamespacedKey key = new NamespacedKey(this, "s");
+        //data.set(key, PersistentDataType.INTEGER, 1);
+        e.getPlayer().sendMessage("" +  entity.getPersistentDataContainer().isEmpty());
     }
 }
