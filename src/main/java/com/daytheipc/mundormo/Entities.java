5de99bf591cd9d6f7348768ec665f1a5d9108b62
DayOS <43686351+Day-OS@users.noreply.github.com>
2022-08-28 20:24:11 -0300

    new imports

--- src/main/java/com/daytheipc/mundormo/Entities.java
+++ src/main/java/com/daytheipc/mundormo/Entities.java
@@ -1,13 +1,41 @@
 package com.daytheipc.mundormo;
 
 import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.bukkit.Bukkit.getLogger;
+import static org.bukkit.Bukkit.getServer;
 
 public class Entities {
+    //DISABLE WANDERER AND VILLAGER
+    //SNOW GOLEMS SHOULD BE DISABLED and instead be just a lifeless mob
+    //CHICKENS MUST NOT DROP EGGS, instead it should follow the same rules as other animals
+    boolean mute;
+    boolean infertile;
+    enum gender{
+        male,
+        female
+    };
+    int hunger;
+    float lifeMultiplier;
+    float speedMultiplier;
+    float attackDamageMultiplier;
+    float jumpHeightMultiplier;
+    float xpMultiplier;
+    float specialDropMultiplier; //cat returning item at night, wool quantity, milk quantity
     float breedingMultiplier;
     float weightMultiplier; //HOW MUCH MEAT does it return after getting killed
 
 
-    public void _AreaCheck(Location location){
-
+    public void _AreaCheck(){
+        Location a = getServer().getWorld("world").getPlayers().get(0).getLocation();
+        getLogger().log(Level.WARNING, "ATIVADO OAAAA BUNDA");
+    }
+    public void MobSpawned(CreatureSpawnEvent e){
+        LivingEntity entity = e.getEntity();
+        entity.setGlowing(true);
     }
 }
--- src/main/java/com/daytheipc/mundormo/Mundormo.java
+++ src/main/java/com/daytheipc/mundormo/Mundormo.java
@@ -1,9 +1,11 @@
 package com.daytheipc.mundormo;
 
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,20 +16,30 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
+import static org.bukkit.Bukkit.getServer;
+
 public final class Mundormo extends JavaPlugin implements Listener {
     Entities entities = new Entities();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         Logger logger = getLogger();
         logger.log(Level.WARNING, "hiii :33");
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                entities._AreaCheck();
+            }
+        }, 0, 2400L);
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void onCreatureSpawn(EntitySpawnEvent e){
-        getLogger().log(Level.INFO, "" + e.getEntity().getEntityId());
+    public void onCreatureSpawn(CreatureSpawnEvent e){
+        if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER_EGG){
+            getLogger().log(Level.INFO, "" + e.getEntity().getEntityId());
+            entities.MobSpawned(e);
+        }
 
     }
     @EventHandler
